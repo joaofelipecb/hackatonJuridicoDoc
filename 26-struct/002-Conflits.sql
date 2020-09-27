0ceb95431e87dc7b3a5d9c04e600c63a322b0cdf
unknown <usuario@W0377699.2g.tj.rs>
2020-09-27 13:58:13 -0300

    reelaboração da tabela conflits, alteração dos status

--- 26-struct/002-Conflits.sql
+++ 26-struct/002-Conflits.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `conflits`;
 CREATE TABLE `conflits` (
 	`conflit_id` INT NOT NULL AUTO_INCREMENT,
-	`conflit_status` ENUM('não classificado','aguardando','resolvido','não resolvido') NULL DEFAULT NULL,
+	`conflit_status` ENUM('cadastrado','desclassificado','classificado','sorteado','resolvido','não resolvido') NULL DEFAULT NULL,
 	`conflit_cod_assunto` INT(11) NULL DEFAULT NULL,
 	`conflit_valor` DECIMAL(10,2) NULL DEFAULT NULL,
 	`conflit_cod_classe_processual` INT(11) NULL DEFAULT NULL,
