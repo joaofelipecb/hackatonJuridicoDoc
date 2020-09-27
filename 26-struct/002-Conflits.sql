3073fbdf1435cdd642efaddb83d31d24a0058aef
unknown <usuario@W0377699.2g.tj.rs>
2020-09-27 04:37:11 -0300

    elaboração do modelo de aprendizagem de máquina não supervisionado e alteração consequente da tabela conflits

--- 26-struct/002-Conflits.sql
+++ 26-struct/002-Conflits.sql
@@ -4,6 +4,8 @@ CREATE TABLE `conflits` (
 	`conflit_status` ENUM('não classificado','aguardando','resolvido','não resolvido') NULL DEFAULT NULL,
 	`conflit_cod_assunto` INT(11) NULL DEFAULT NULL,
 	`conflit_valor` DECIMAL(10,2) NULL DEFAULT NULL,
+	`conflit_cod_classe_processual` INT(11) NULL DEFAULT NULL,
+	`conflit_processo_prioritario` ENUM('S','N') NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
 	PRIMARY KEY (`conflit_id`)
 )
 COLLATE='utf8mb4_general_ci'
