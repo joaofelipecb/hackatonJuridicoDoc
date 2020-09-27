64daf3bdf3a4b6eb5aaca3569c12faad162e3715
unknown <usuario@W0377699.2g.tj.rs>
2020-09-27 00:43:31 -0300

    SQL da nova criação da tabela conflits e novo arquivo de populacao

--- 26-struct/002-Conflits.sql
+++ 26-struct/002-Conflits.sql
@@ -1,6 +1,9 @@
+DROP TABLE IF EXISTS `conflits`;
 CREATE TABLE `conflits` (
 	`conflit_id` INT NOT NULL AUTO_INCREMENT,
-	`conflit_status` ENUM('aguardando','resolvido','não resolvido') NULL DEFAULT NULL,
+	`conflit_status` ENUM('não classificado','aguardando','resolvido','não resolvido') NULL DEFAULT NULL,
+	`conflit_cod_assunto` INT(11) NULL DEFAULT NULL,
+	`conflit_valor` DECIMAL(10,2) NULL DEFAULT NULL,
 	PRIMARY KEY (`conflit_id`)
 )
 COLLATE='utf8mb4_general_ci'
--- 30-data/001-Conflits.sql
+++ 30-data/001-Conflits.sql
@@ -1,5 +1,4 @@
-INSERT INTO `hackaton`.`conflits` (`conflit_status`) VALUES ('aguardando');
-INSERT INTO `hackaton`.`conflits` (`conflit_status`) VALUES ('aguardando');
-INSERT INTO `hackaton`.`conflits` (`conflit_status`) VALUES ('aguardando');
-INSERT INTO `hackaton`.`conflits` (`conflit_status`) VALUES ('aguardando');
-INSERT INTO `hackaton`.`conflits` (`conflit_status`) VALUES ('aguardando');
+TRUNCATE TABLE `hackaton`.`conflits`;
+INSERT INTO `hackaton`.`conflits` (`conflit_status`, `conflit_cod_assunto`, `conflit_valor`) VALUES ('não classificado', '11884', '1000');
+INSERT INTO `hackaton`.`conflits` (`conflit_status`, `conflit_cod_assunto`, `conflit_valor`) VALUES ('não classificado', '11884', '1200');
+INSERT INTO `hackaton`.`conflits` (`conflit_status`, `conflit_cod_assunto`, `conflit_valor`) VALUES ('não classificado', '11884', '15000');
